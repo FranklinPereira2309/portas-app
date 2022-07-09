df0a01c5411d5d9de0e7714a3e51549d8367bbb0
fmpereira2309 <fmpereira2309@gmail.com>
2022-07-08 21:18:21 -0300

    Criação da função criar array de portas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import Porta from "../components/Porta"
+import { criarPortas } from "../functions/portas"
 import PortaModel from "../model/porta"
 
 export default function Home() {
 
   const [p1, setP1] = useState(new PortaModel(1))
 
-  return (
+    return (
     <div style={{ display: 'flex' }}>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)
       }/>
